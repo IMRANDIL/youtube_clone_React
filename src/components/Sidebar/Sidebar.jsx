6c014184feb0905820141e61fb30fe6e87671613
IMRANDIL <aliimranadil2@gmail.com>
2022-03-27 10:55:39 +0530

    yh

--- src/components/Sidebar/Sidebar.jsx
+++ src/components/Sidebar/Sidebar.jsx
@@ -4,6 +4,15 @@ import './Sidebar.css'
 import HomeSharpIcon from '@mui/icons-material/HomeSharp';
 import WhatshotSharpIcon from '@mui/icons-material/WhatshotSharp';
 import SubscriptionsSharpIcon from '@mui/icons-material/SubscriptionsSharp';
+import VideoLibrarySharpIcon from '@mui/icons-material/VideoLibrarySharp';
+import HistorySharpIcon from '@mui/icons-material/HistorySharp';
+import OndemandVideoSharpIcon from '@mui/icons-material/OndemandVideoSharp';
+import WatchLaterSharpIcon from '@mui/icons-material/WatchLaterSharp';
+import ThumbUpAltSharpIcon from '@mui/icons-material/ThumbUpAltSharp';
+import ExpandMoreSharpIcon from '@mui/icons-material/ExpandMoreSharp';
+
+
+
 
 
 
@@ -18,8 +27,13 @@ const Sidebar = () => {
             <SidebarRow selected title='Home' Icon={HomeSharpIcon} />
             <SidebarRow title='Trending' Icon={WhatshotSharpIcon} />
             <SidebarRow title='Subscription' Icon={SubscriptionsSharpIcon} />
-
-
+            <hr />
+            <SidebarRow title='Library' Icon={VideoLibrarySharpIcon} />
+            <SidebarRow title='History' Icon={HistorySharpIcon} />
+            <SidebarRow title='Your videos' Icon={OndemandVideoSharpIcon} />
+            <SidebarRow title='Watch Later' Icon={WatchLaterSharpIcon} />
+            <SidebarRow title='Liked Videos' Icon={ThumbUpAltSharpIcon} />
+            <SidebarRow title='Show more' Icon={ExpandMoreSharpIcon} />
         </div>
     )
 }
