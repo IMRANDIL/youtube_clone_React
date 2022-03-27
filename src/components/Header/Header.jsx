44f2ff79a9862c6ab76ec88cb16d13638a553b97
IMRANDIL <aliimranadil2@gmail.com>
2022-03-27 07:31:10 +0530

    yh

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,24 @@
 import React from 'react'
 import './Header.css'
 
+import MenuSharpIcon from '@mui/icons-material/MenuSharp';
+import { Avatar, IconButton } from '@mui/material';
+
+import logo from '../../img/youtube.gif'
+import SearchSharpIcon from '@mui/icons-material/SearchSharp';
+import VideoCallSharpIcon from '@mui/icons-material/VideoCallSharp';
+import AppsSharpIcon from '@mui/icons-material/AppsSharp';
+import NotificationsActiveSharpIcon from '@mui/icons-material/NotificationsActiveSharp';
+
+
+
+
+
+
+
+
+
+
 
 
 
@@ -8,7 +26,30 @@ const Header = () => {
     return (
         <div className='header'>
 
+            <div className="header__left">
+                <IconButton>
+                    <MenuSharpIcon />
 
+                </IconButton>
+
+                <img src={logo} alt="logo-img" className='header__logo' />
+
+            </div>
+
+            <div className="header__middle">
+                <input type="text" />
+                <SearchSharpIcon />
+            </div>
+
+
+            <div className="header__right">
+
+                <VideoCallSharpIcon />
+                <AppsSharpIcon />
+                <NotificationsActiveSharpIcon />
+                <Avatar src='https://avatars.githubusercontent.com/u/71559091?s=400&u=261c7198a75f868c1d0c5deda860c96ad06d8820&v=4' alt='profile-img' />
+
+            </div>
 
 
         </div>
